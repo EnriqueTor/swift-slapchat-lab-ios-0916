c11f4d6420939f6644aae2c2216a2d6e8df3d2b6
EnriqueTor <etorrendell@gmail.com>
2016-11-10 14:06:38 -0500

    Complete

--- SlapChat/DataStore.swift
+++ SlapChat/DataStore.swift
@@ -15,6 +15,8 @@ class DataStore {
     
     private init() {}
     
+    var messages: [Message] = []
+    
     // MARK: - Core Data stack
     
     lazy var persistentContainer: NSPersistentContainer = {
@@ -47,10 +49,15 @@ class DataStore {
     // MARK: - Core Data Saving support
     
     func saveContext () {
+        
         let context = persistentContainer.viewContext
+        
         if context.hasChanges {
+            
             do {
+                
                 try context.save()
+                
             } catch {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
@@ -59,5 +66,64 @@ class DataStore {
             }
         }
     }
+    
+    func fetchData() {
+        
+        let managedContext = persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<Message>(entityName: "Message")
+        
+        // let fetchRequest = NSFetchRequest<Message> = Message.fetchRequest()
+        
+        do {
+            
+            let unSortedMessages = try managedContext.fetch(fetchRequest)
+            
+            
+            self.messages = unSortedMessages.sorted(by: { (message1, message2) -> Bool in
+//                return message1.createdAt?.compare(message2.createdAt as Date!) == ComparisonResult.orderedAscending
+                
+             let dateA = message1.createdAt as! Date
+             let dateB = message2.createdAt as! Date
+            
+            return dateA > dateB
+
+            })
+
+            
+        } catch {
+            
+            print("error")
+        }
+            
+        
+    }
+    
+    func generateTestData() {
+
+        print("4. we are creating crap number 1")
+        let message1 = NSEntityDescription.insertNewObject(forEntityName: "Message", into: self.persistentContainer.viewContext) as! Message
+        message1.content = "We are so cool"
+        message1.createdAt = NSDate()
+        
+        print("4. we are creating crap number 2")
+        let message2 = NSEntityDescription.insertNewObject(forEntityName: "Message", into: self.persistentContainer.viewContext) as! Message
+        message2.content = "We are so cool too"
+        message2.createdAt = NSDate()
+        
+        let message3 = NSEntityDescription.insertNewObject(forEntityName: "Message", into: self.persistentContainer.viewContext) as! Message
+        message3.content = "We are super super coooooooool"
+        message3.createdAt = NSDate()
+        
+        let message4 = Message(context: persistentContainer.viewContext)
+        message4.content = "What!!??!?! This is soooooo coooooool!"
+        message4.createdAt = NSDate()
+        
+        
+        saveContext()
+        fetchData()
+        
+    }
+    
     
 }
--- SlapChat/TableViewController.swift
+++ SlapChat/TableViewController.swift
@@ -7,17 +7,91 @@
 //
 
 import UIKit
+import CoreData
 
 class TableViewController: UITableViewController {
+    
+    var store = DataStore.sharedInstance
 
     override func viewDidLoad() {
-        super.viewDidLoad()
+      super.viewDidLoad()
+        
+        store.fetchData()
+        print("1. we are bringing the data")
+        
+        if store.messages.isEmpty {
+            print("2. ey! this was empty so we are putting crap inside")
+            generateTestData()
+            print("we just finished all the crap")
+        }
+        
+        self.tableView.reloadData()
         
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        store.fetchData()
+        self.tableView.reloadData()
+    }
+    
+    func generateTestData() {
+        print("3. we started printing crap")
+        store.generateTestData()
+        store.saveContext()
+        store.fetchData()
+        print("we finished printing crap")
         
     }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return store.messages.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "messageCell", for: indexPath)
+        
+        
+        cell.textLabel?.text = store.messages[indexPath.row].content
+        
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "segueDetail" { return }
+        
+        
+    }
+    
+}
+
+class addMessageViewController: UIViewController {
+    
+    let context = DataStore.sharedInstance.persistentContainer.viewContext
+    
+    
+    @IBOutlet weak var text: UITextField!
+    
+    @IBAction func saveButton(_ sender: UIButton) {
+   
+        let message = NSEntityDescription.insertNewObject(forEntityName: "Message", into: context) as! Message
+        message.content = text.text
+        message.createdAt = NSDate()
+        
+        DataStore.sharedInstance.messages.append(message)
+        
+        navigationController?.popViewController(animated: true)
+        }
+    
+    
+    
+    
 }
